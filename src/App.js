e92b89b866a5a056b9b08d9551aac7f9008f9dea
George Flores <georgeflores886@gmail.com>
2021-05-26 10:11:39 -0400

    created global styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Switch, Route, useLocation } from 'react-router-dom'
 
+import GlobalStyle from './components/GlobalStyle'
 function App() {
+  const location = useLocation()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyle />
+      <h1>Hello from react</h1>
     </div>
   );
 }
---
+++ src/components/GlobalStyle.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { createGlobalStyle } from 'styled-components'
+const GlobalStyle = createGlobalStyle`
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+html {
+    @media (max-width: 1700px){
+        font-size: 75%;
+    }
+}
+body {
+    background: #1b1b1b;
+    font-family: 'Noto Sans', sans-serif;
+    overflow-x: hidden;
+    color: #fff;
+}
+
+`
+
+export default GlobalStyle
