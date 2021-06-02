9d511fc5ac3951c14351603121254988f304a5b9
George Flores <georgeflores886@gmail.com>
2021-06-02 10:04:56 -0400

    created gameplay 1 component

---
+++ src/components/PE1Gameplay.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Row, SPAN_1_OF_3 } from '../styles'
+
+const PE1Gameplay = () => {
+    return (
+        <div>
+            <section>
+                <h2>Parasite Eve I Gameplay</h2>
+            </section>
+        </div>
+    )
+}
+
+export default PE1Gameplay
--- src/pages/Gameplay.js
+++ src/pages/Gameplay.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
+import PE1Gameplay from '../components/PE1Gameplay'
 
 const Gameplay = () => {
     return (
         <div>
             <StyledHeader>
-                <h1>Gameplay</h1>
+                <StyledHeroText>
+                    <h1>Gameplay</h1>
+                </StyledHeroText>
             </StyledHeader>
+            <PE1Gameplay />
         </div>
     )
 }
@@ -14,5 +18,14 @@ const Gameplay = () => {
 const StyledHeader = styled.header`
     height: 80vh;
     background: linear-gradient(171deg, rgba(187,190,187,1) 17%, rgba(104,170,157,0.9612219887955182) 54%, rgba(213,219,215,1) 86%);
+`
+
+const StyledHeroText = styled.div`
+    position: absolute;
+    top: 30%;
+    left: 15%;
+    h1 {
+        font-size: 7rem;
+    }
 `
 export default Gameplay
