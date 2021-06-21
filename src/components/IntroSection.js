22f1532f95b1a66dd45d3da4ffdafe5197043c4a
George Flores <georgeflores886@gmail.com>
2021-06-21 14:13:17 -0400

    added animation in intro section

--- src/components/IntroSection.js
+++ src/components/IntroSection.js
@@ -2,40 +2,40 @@ import React from 'react'
 import styled from 'styled-components'
 import { motion } from 'framer-motion'
 import { InView } from 'react-intersection-observer'
-import { Row, SPAN_1_OF_2, SPAN_2_OF_2 } from '../styles'
-import { useScroll } from './useScroll'
+import { Row, SPAN_1_OF_2 } from '../styles'
 import Cover from '../img/PE1.png'
 import introImg from '../img/intro.jpg'
 import eve1 from '../img/eve1.jpg'
 import eve2 from '../img/eve2.jpg'
 
-const line1 = 'Experience the classic horror RPG'
-const line2 = 'Experience the duology of Aya Brea who possesses abnormal abilities due to changes in her mitochondria.'
+const headLine = 'Experience the classic horror RPG'
+const subtitleLine = 'Experience the duology of Aya Brea who possesses abnormal abilities due to changes in her mitochondria.'
+const gameHeadLine = 'Parasite Eve I & Parasite Eve II'
 
 const IntroSection = () => {
     return (
         <>
             <StyledIntro>
                 <Row>
-                    <InView threshold={0.3} triggerOnce={null}>
+                    <InView threshold={0.25} triggerOnce>
                         {({ ref, inView }) => (
                             <StyledContext>
                                 <motion.h2 ref={ref} variants={introSentence} initial='hidden' animate={`${inView ? 'visible' : 'hidden'}`}>
-                                    {line1.split('').map((char, idx) => {
+                                    {headLine.split('').map((char, idx) => {
                                         return (
                                             <motion.span key={char + "-" + idx} variants={letter}>{char}</motion.span>
                                         )
                                     })}
                                 </motion.h2>
                                 <motion.p ref={ref} variants={introSentence} initial='hidden' animate={`${inView ? 'visible' : 'hidden'}`}>
-                                    {line2.split('').map((char, idx) => {
+                                    {subtitleLine.split('').map((char, idx) => {
                                         return (
                                             <motion.span key={char + '-' + idx} variants={letter}>{char}</motion.span>
                                         )
                                     })}
                                 </motion.p>
 
-                                <motion.StyledIntroImage></motion.StyledIntroImage>
+                                <StyledIntroImage ref={ref} initial='hidden' transition={{ duration: 0.6 }} animate={`${inView ? 'visible' : 'hidden'}`} variants={introImageVar} ></StyledIntroImage>
 
                             </StyledContext>
                         )}
@@ -59,19 +59,40 @@ const IntroSection = () => {
                     <Row>
                         <h2>Parasite Eve I & Parasite Eve II</h2>
                     </Row>
-                    <Row>
-                        <BoxImg>
-                            <img src={eve1} alt='cutscene' />
-                            <h3>Parasite Eve I</h3>
-                            <p>In 1997, NYPD officer Aya Brea attempts to stop Eve, a mysterious woman who plans to eradicate humanity through spontaenous combustion.</p>
-                        </BoxImg>
-                        <BoxImg>
-                            <img src={eve2} alt='cutscene' />
-                            <h3>Parasite Eve II</h3>
-                            <p>Three years after the events of <em>Parasite Eve I</em>, Aya is pulled into another adventure where enemies she previously encountered are reportedly sighted in the American Southwest.</p>
-                        </BoxImg>
-
-                    </Row>
+                    <InView threshold={0.15} triggerOnce>
+                        {({ ref, inView }) => (
+                            <Row>
+                                <BoxImg>
+                                    <motion.img ref={ref}
+                                        initial='hidden'
+                                        transition={{ duration: 0.6 }}
+                                        animate={`${inView ? 'visible' : 'hidden'}`}
+                                        variants={introImageVar}
+                                        whileTap={{ scale: 0.8 }}
+                                        src={eve1}
+                                        alt='cutscene' />
+                                    <h3>Parasite Eve I</h3>
+                                    <motion.p animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ ease: "easeIn", duration: 2 }}>
+                                        In 1997, NYPD officer Aya Brea attempts to stop Eve, a mysterious woman who plans to eradicate humanity through spontaenous combustion.
+                                    </motion.p>
+                                </BoxImg>
+                                <BoxImg>
+                                    <motion.img ref={ref}
+                                        initial='hidden'
+                                        transition={{ duration: 0.6 }}
+                                        animate={`${inView ? 'visible' : 'hidden'}`}
+                                        variants={introImageVar}
+                                        whileTap={{ scale: 0.8 }}
+                                        src={eve2}
+                                        alt='cutscene' />
+                                    <h3>Parasite Eve II</h3>
+                                    <motion.p animate={{ opacity: 1 }} initial={{ opacity: 0 }} transition={{ ease: "easeIn", duration: 2 }}>
+                                        Three years after the events of <em>Parasite Eve I</em>, Aya is pulled into another adventure where enemies she previously encountered are reportedly sighted in the American Southwest.
+                                    </motion.p>
+                                </BoxImg>
+                            </Row>
+                        )}
+                    </InView>
                 </Row>
             </StyledDuo>
         </>
@@ -95,6 +116,11 @@ const letter = {
         y: 0
     }
 }
+const introImageVar = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 }
+}
+
 
 const StyledContext = styled.div`
     margin-bottom: 20px;
@@ -102,7 +128,7 @@ const StyledContext = styled.div`
         text-align: center;
     }
 `
-const StyledIntroImage = styled.div`
+const StyledIntroImage = styled(motion.div)`
     margin: 1.9rem 0 1.5rem 0; 
     background-image: url(${introImg});
     width: 100%;
